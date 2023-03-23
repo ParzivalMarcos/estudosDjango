7110aac021362d1b135e987727bedfb72dea65ca
Marcos Lima <marcos.lima@digibee.com>
2023-03-23 12:08:40 -0300

    Configurando inicio da pagina admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+
+    ## Deixar os campos como links
+    list_display_links = ("id", "nome")
+
+    ## Adicionar campo de busca
+    search_fields = ("nome",)
+
+admin.site.register(Fotografia, ListandoFotografias)
